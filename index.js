bebcc1c5cbd5dc06d882a09274c9ba3011d903bc
vkub1 <kubliyvlad@gmail.com>
2021-10-04 11:35:03 -0700

    Part 2 SignIn complete

--- index.js
+++ index.js
@@ -3,12 +3,47 @@ const app = express();
 
 const logger = require('morgan');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 app.use(logger('dev'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use((req,res, next) => {
+    const username = req.cookies.username || '';
+    res.locals.username = username;
+    next();
+})
+
+app.get('/', (req, res) => {
+    if (req.cookies.username) {
+        res.render('index')
+    } else {
+        res.redirect('/signin')
+    }
+})
+
+app.get('/signin', (req, res) => {
+    let username = req.cookies.username || "";
+    res.render('sign-in', {
+        username: username
+    })
+})
+
+app.post('/signin', (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect('/');
+})
+
+app.post('/sign_out', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('/signin');
+})
+
 
 
 const PORT = 3000;
